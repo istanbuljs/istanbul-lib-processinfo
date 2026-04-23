b9da129dab494c1e8514dbae963202e9cbbda375
mhassan1 <marc.j.hassan@gmail.com>
2026-04-23 13:12:26 -0400

    fix(deps): remove vulnerable dependency `uuid`

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-const { v4: uuidv4 } = require('uuid');
 const archy = require('archy')
 const libCoverage = require('istanbul-lib-coverage')
 const {dirname, resolve} = require('path')
@@ -50,7 +49,7 @@ class ProcessInfo {
     Object.assign(this, defaults(), fields)
 
     if (!this.uuid) {
-      this.uuid = uuidv4()
+      this.uuid = crypto.randomUUID()
     }
   }
 
